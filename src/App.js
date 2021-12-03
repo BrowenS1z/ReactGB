e4a647cff30677445ec6b41857aa360d1fc419cd
BrowenSiz <BrowenSiz@yandex.ru>
2021-12-03 18:14:56 +0300

    L2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Message } from "./components/Message";
-
+//import { Message } from './components/Message/Message'
+import { Form } from './components/Form'
 
 function App() {
-    let text = "Hello";
-  return (
-      <div className="App">
-        <header className="App-header">
-          <Message message = {text} />
-        </header>
-      </div>
-  );
+    const [messages, setMessages] = useState([]);
+
+    const handleMessage = (text) => {
+        if (messages.length === 0) {
+            setMessages([{ id: 0, text: text, author: 'User' }]);
+        }
+        else {
+            setMessages([...messages, { id: messages[messages.length - 1].id + 1, text: text, author: 'User' }]);
+        }
+    }
+
+    useEffect(() => {
+        if (messages.length !== 0 && messages[messages.length - 1].author === "User") {
+            setTimeout(() => {
+                setMessages([...messages, { id: messages[messages.length - 1].id + 1, text: "Привет! Я бот.", author: 'Bot' }])
+            }, 1500)
+        }
+    }, [messages])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Form sendMessage={handleMessage} />
+                {messages.map((message) => <div key={message.id}><span className="author-name">{message.author}: </span>{message.text}</div>)}
+            </header>
+        </div>
+    );
 }
 
 export default App;
